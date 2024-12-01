143811757de65f0e9556dc13547ccf1fc977e1f6
GeordieEK <geordieek@gmail.com>
2024-12-01 21:56:49 +1100

    refactor: general day01 cleanup

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -1,24 +1,24 @@
-import { readFileLineByLine as createReadLineInterface } from "../utils/utils.ts";
+import { createReadLineInterface } from "../utils/utils.ts";
 
-// https://adventofcode.com/2024/day/1
+// Problem from: https://adventofcode.com/2024/day/1
+const INPUT_FILE = "src/day01/day01.input";
 
 async function main() {
   // readLineInterface allows us to read the file line by line
-  const readLineInterface = await createReadLineInterface(
-    "src/day01/day01.input",
-  );
+  const readLineInterface = await createReadLineInterface(INPUT_FILE);
   const list1 = [],
     list2 = [];
   // Read the input line by line
   for await (const line of readLineInterface) {
     // Input data is 2 lists side by side
     // Text on left will be list1, right will be list2
-    const lineArr = line.trim().split("  ");
+    const lineArr = line.trim().split(/\s+/); // Split on one or more spaces
     list1.push(parseInt(lineArr[0]));
     list2.push(parseInt(lineArr[1]));
   }
   // Sort both lists, so we're matching smallest with smallest
   // TODO: Potential optimisation, could push values into heaps as they come in to sort
+  // NOTE: This mutates the arrays, which generally isn't ideal but is fine for this use case.
   list1.sort();
   list2.sort();
   // We want the total distance between each pair of values from the sorted lists
